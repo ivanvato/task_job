c85d3f0b631a87da9d09b98e7ad9ceb0ee308348
ivanvato <39733336+ivanvato@users.noreply.github.com>
2018-05-31 16:49:17 +0300

    Added right normalize.sql

--- normalize.sql
+++ normalize.sql
@@ -7,27 +7,45 @@ INSERT INTO user(id, name, lang, location)
 SELECT DISTINCT user_id, name, lang, location
 FROM tweet;
 
+CREATE TABLE IF NOT EXISTS media(id INTEGER PRIMARY KEY,
+                  display_url TEXT);
+
+INSERT INTO media(display_url)
+SELECT DISTINCT display_url
+FROM tweet
+WHERE display_url like 'p%'
+ORDER BY tweet_id;
+
 ALTER TABLE tweet RENAME TO tweet_old;
 
-CREATE TABLE IF NOT EXISTS tweet(id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        tweet_id INTEGER,
+ALTER TABLE tweet_old ADD COLUMN media_id INTEGER DEFAULT NULL;
+
+CREATE TABLE IF NOT EXISTS tweet(id INTEGER PRIMARY KEY,
                         user_id INTEGER,
                         tweet_text TEXT,
+                        media_id INTEGER DEFAULT NULL,
                         country_code TEXT,
-                        display_url TEXT,
-                        created_at TIMESTAMP,                        
+                        created_at TIMESTAMP,
                         tweet_sentiment INTEGER,
-                        FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE ON UPDATE NO ACTION);
+                        FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE ON UPDATE NO ACTION
+                        FOREIGN KEY (media_id) REFERENCES media(id) ON DELETE CASCADE ON UPDATE NO ACTION);
 
-INSERT INTO tweet(tweet_id, user_id, tweet_text, country_code, display_url, created_at, tweet_sentiment)
-       select tweet_id, 
-              user_id, 
-              tweet_text, 
-              country_code, 
-              display_url, 
-              created_at,              
+UPDATE tweet_old
+SET media_id=(SELECT id FROM media WHERE tweet_old.display_url=media.display_url);
+
+pragma writable_schema=1;
+update SQLITE_MASTER set sql = replace(sql, 'FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE ON UPDATE NO ACTION)',
+    'FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE ON UPDATE NO ACTION FOREIGN KEY (media_id) REFERENCES media(id) ON DELETE CASCADE ON UPDATE NO ACTION)'
+) where name = 'tweet_old' and type = 'table';
+
+INSERT INTO tweet(id, user_id, tweet_text, country_code, media_id, created_at, tweet_sentiment)
+       SELECT DISTINCT tweet_id,
+              user_id,
+              tweet_text,
+              country_code,
+              media_id,
+              created_at,
               tweet_sentiment
-       from tweet_old;
+       FROM tweet_old;
 
 DROP TABLE tweet_old;
-
