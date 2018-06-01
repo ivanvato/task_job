45e75713fed5ad71884c818cc582971d338dc583
ivanvato <39733336+ivanvato@users.noreply.github.com>
2018-06-01 10:27:00 +0300

    query with window function

--- sql_for_table.sql
+++ sql_for_table.sql
@@ -5,5 +5,5 @@ select id,
 			value, 
 			date, 
 			max(value) over (partition by id) as max_value, 
-			max(date) over (partition by id) as max_date from t) as calculated_table 
+			max(date) over (partition by id, value) as max_date from t) as calculated_table 
 	where max_date=date and max_value=value;
