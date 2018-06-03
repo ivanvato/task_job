b9ec36c73584bed42031f1458f5329b848c5f54d
ivanvato <39733336+ivanvato@users.noreply.github.com>
2018-06-03 12:15:12 +0300

    Update sql_for_table.sql

--- sql_for_table.sql
+++ sql_for_table.sql
@@ -4,6 +4,6 @@ select id, value, date
 			value, 
 			date, 
 			max(date) over (partition by id) as max_date, 
-			max(value) over (partition by id, date order by id, date) as max_value 
+			max(value) over (partition by id, date) as max_value 
 	from t) as calculated_table 
 	where max_date=date and max_value=value;
